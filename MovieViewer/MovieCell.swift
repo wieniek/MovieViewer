51cc6e0d7589762ac9f6e04ecfcce10cb392c224
Wieniek Sliwinski <wieniek@gmail.com>
2017-03-29 09:31:27 +0100

    End of Custom Cells Walkthrough

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Wieniek Sliwinski on 3/29/17.
+//  Copyright © 2017 Home User. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var overviewLabel: UILabel!
+  
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MovieViewer/MoviesViewController.swift
@@ -0,0 +1,89 @@
+//
+//  MoviesViewController.swift
+//  MovieViewer
+//
+//  Created by Wieniek Sliwinski on 3/28/17.
+//  Copyright © 2017 Home User. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+  
+  @IBOutlet weak var tableView: UITableView!
+  
+  // array of dictionaries which represents each movie
+  // retrieved with URLSession network request
+  var movies: [NSDictionary]?
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // set MoviesViewController as TableView DataSource and Delegate
+    tableView.dataSource = self
+    tableView.delegate = self
+    
+    // network request using URLSession
+    let apiKey = "f369874e61746bfeb1fce02b24b3a5cb"
+    let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")!
+    let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+    let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+    let task: URLSessionDataTask = session.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+      if let error = error {
+        //errorCallBack?(error)
+        print("ERROR = \(error)")
+      } else if let data = data,
+        let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? NSDictionary {
+        print("JSON OKAY")
+        //print(dataDictionary)
+        // load results to movies variable
+        self.movies = dataDictionary["results"] as? [NSDictionary]
+        self.tableView.reloadData()
+        //successCallBack(dataDictionary)
+      }
+    }
+    task.resume()
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  // implement UITableViewDataSource required methods
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // set number of rows in table view
+    if let movies = movies {
+      return movies.count
+    } else {
+      return 0
+    }
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    // set content of individual cell
+    let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+    
+    // get movie corresponding to indexPath from the movies array
+    let movie = movies![indexPath.row]
+    // get data and set the labeles
+    let title = movie["title"] as! String
+    let overview = movie["overview"] as! String
+    cell.titleLabel.text = title
+    cell.overviewLabel.text = overview
+    return cell
+  }
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destinationViewController.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
